03cdf1fcdb6e8ae324784dd599b1d7aec4a40828
Tatana <burmatova@inbox.ru>
2020-08-27 20:04:28 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,23 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentRadioStation;
+    private int soundVolume;
+
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+
+    public void setSoundVolume(int soundVolume) {
+        this.soundVolume = soundVolume;
+    }
+}
